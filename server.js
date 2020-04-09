c63b1ee69371119ca6b3d1b5cf70d860108e283a
Iago <yagomello16@gmail.com>
2020-04-09 01:16:26 -0300

    finished challenge

--- server.js
+++ server.js
@@ -2,48 +2,85 @@
 
 const axios = require('axios');
 const fs = require('fs');
-
-const url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=b0b5a0bd9a583856db61e4f7e1d90ec1a2d6289e';
+const crypto = require('crypto');
+const FormData = require('form-data');
+const JSONStream = require('JSONStream');
+const api = 'https://api.codenation.dev/v1/challenge/dev-ps';
+const token = 'b0b5a0bd9a583856db61e4f7e1d90ec1a2d6289e';
 
 (async function main() {
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(api + `/generate-data?token=${token}`);
     saveAnswer(data);
     decryptAnswer(data);
-    // sendAnswer();
-  } catch (error) {
-    throw error;
+    hashAnswer();
+  } catch ({ response }) {
+    console.log(response.data.message);
   }
 })();
 
 function saveAnswer(answer) {
   const stringfiedData = JSON.stringify(answer, null, 2);
-  fs.writeFile('answer.json', stringfiedData, function writeJSON(err) {
-    if (err) return console.log(err);
+  fs.writeFileSync('answer.json', stringfiedData);
+}
+
+async function sendAnswer() {
+  const form = new FormData();
+  form.append('answer', fs.createReadStream('answer.json'));
+
+  try {
+    const res = await axios.post(api + `/submit-solution?token=${token}`, form, {
+      headers: form.getHeaders(),
+    });
+    console.log('data sent response: ', res);
+  } catch ({ response }) {
+    console.error(response.data.message);
+  }
+}
+
+function hashAnswer() {
+  fs.readFile('answer.json', async (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const answer = JSON.parse(data);
+
+    answer.resumo_criptografico = await crypto
+      .createHash('sha1')
+      .update(answer.decifrado)
+      .digest('hex');
+
+    await saveAnswer(answer);
+
+    sendAnswer();
   });
 }
 
-function sendAnswer() {}
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'];
-
-function decryptAnswer(answer) {
+async function decryptAnswer(answer) {
   const { numero_casas, cifrado } = answer;
+
   const REGEX_ALPHABET = /[a-z]/i;
-  let decryptedAnswer = '';
   let answerDictionary = {};
-  for (let cifra of cifrado) {
+  const cryptedValue = cifrado.split('');
+
+  const decryptedAnswer = cryptedValue.reduce((decrypted, cifra) => {
     if (!REGEX_ALPHABET.test(cifra)) {
-      decryptedAnswer += cifra;
-    } else if (answerDictionary[cifra]) {
-      decryptedAnswer += answerDictionary[cifra];
-    } else {
-      const indexValorReal = alphabet.indexOf(cifra) - numero_casas;
-      const valorReal = indexValorReal < 0 ? alphabet[alphabet.length + indexValorReal] : alphabet[indexValorReal];
-      decryptedAnswer += valorReal;
-      answerDictionary[cifra] = valorReal;
+      return decrypted + cifra;
     }
-  }
+    if (answerDictionary[cifra]) {
+      return decrypted + answerDictionary[cifra];
+    }
+
+    const decryptedValueIndex = alphabet.indexOf(cifra) - numero_casas;
+    const valorReal = decryptedValueIndex < 0 ? alphabet[alphabet.length + decryptedValueIndex] : alphabet[decryptedValueIndex];
+    answerDictionary[cifra] = valorReal;
+    return decrypted + valorReal;
+  }, '');
+
   saveAnswer({
     ...answer,
     decifrado: decryptedAnswer,
